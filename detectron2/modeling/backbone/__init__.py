e332f2941c2e7a6e7eaf52c73e23a37f908d2a38
haomiao <haomiao@haomiaodeMacBook-Pro.local>
2021-01-27 21:31:23 +0800

    update

--- detectron2/modeling/backbone/__init__.py
+++ detectron2/modeling/backbone/__init__.py
@@ -4,6 +4,7 @@ from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
 from .backbone import Backbone
 from .fpn import FPN
 from .resnet import ResNet, ResNetBlockBase, build_resnet_backbone, make_stage
+from .condconv_resnet import build_cond_conv_resnet_backbone
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
 # TODO can expose more resnet blocks after careful consideration
