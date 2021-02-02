8531afb781c432c75b146f64907e777f6ae4713d
haomiao <haomiao@haomiaodeMacBook-Pro.local>
2021-02-02 21:35:11 +0800

    update

--- playground/meta_hard_generate/network.py
+++ playground/meta_hard_generate/network.py
@@ -206,9 +206,7 @@ class MyNetwork(nn.Module):
             second = time_captured.second
             meta = (((second / 60 + minute) / 60) + hour) / 24
             metas.append(meta)
-        metas = torch.Tensor(metas)
-        metas = metas.
-        metas = , dtype=images.tensor.dtype, device=images.tensor.device)
+        metas = torch.tensor(metas, dtype=images.tensor.dtype, device=images.tensor.device)
 
         features = self.backbone(images.tensor, metas)
 
