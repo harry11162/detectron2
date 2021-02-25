b9698203327ed840e5104fcbbc2b129a14ddde89
haomiao <mhao@ucsd.edu>
2021-02-25 18:54:30 +0800

    update

--- playground/meta_hard_generate_encoding/network.py
+++ playground/meta_hard_generate_encoding/network.py
@@ -15,7 +15,7 @@ from detectron2.utils.logger import log_first_n
 
 from detectron2.layers import ShapeSpec
 
-from detectron2.modeling.backbone.meta_condconv_resnet import build_meta_cond_conv_resnet_backbone
+from detectron2.modeling.backbone.mobilenet import build_mobilenetv2_backbone
 from detectron2.modeling.backbone import Backbone, build_backbone
 from detectron2.modeling.backbone.fpn import build_custom_backbone_fpn
 from detectron2.modeling.postprocessing import detector_postprocess
@@ -50,7 +50,7 @@ class MyNetwork(nn.Module):
             vis_period: the period to run visualization. Set to 0 to disable.
         """
         super().__init__()
-        backbone = build_meta_cond_conv_resnet_backbone(cfg, ShapeSpec(channels=3))
+        backbone = build_mobilenetv2_backbone(cfg)
         self.backbone = build_custom_backbone_fpn(cfg, backbone)
         self.proposal_generator = build_proposal_generator(cfg, self.backbone.output_shape())
         self.roi_heads = build_roi_heads(cfg, self.backbone.output_shape())
