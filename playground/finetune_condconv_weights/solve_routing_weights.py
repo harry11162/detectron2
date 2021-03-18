e745dce70e2744f18484f8deff537cc7940b50f8
haomiao <mhao@ucsd.edu>
2021-03-19 00:12:12 +0800

    udpate

--- playground/finetune_condconv_weights/solve_routing_weights.py
+++ playground/finetune_condconv_weights/solve_routing_weights.py
@@ -144,6 +144,8 @@ def do_train(cfg, model, resume=False):
             print("found")
             assert False, "found"
             params = params + list(m.parameters())
+    print(params)
+    return 0
     optimizer = torch.optim.SGD(params, lr=cfg.SOLVER.BASE_LR,
                                 momentum=cfg.SOLVER.MOMENTUM, weight_decay=0)
 
