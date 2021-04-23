d712f66fa18d9529d2a371c78b9a8a53b033bdad
haomiao <mhao@ucsd.edu>
2021-04-23 11:06:43 +0800

    update

--- playground/get_condconv_weights/solve_routing_weights.py
+++ playground/get_condconv_weights/solve_routing_weights.py
@@ -162,8 +162,8 @@ def setup(args):
     register_coco_instances("domain", {}, "domain/annotations.json", "domain")
     register_coco_instances("domain_train", {}, "domain/train_annotations.json", "domain")
     register_coco_instances("domain_test", {}, "domain/test_annotations.json", "domain")
-    register_coco_instances("routine_train", {}, "domain/test_routine_5fc877.json", "domain")
-    register_coco_instances("routine_test", {}, "domain/test_routine_5fc877.json", "domain")
+    register_coco_instances("routine_train", {}, "domain/train_routine_5fc766.json", "domain")
+    register_coco_instances("routine_test", {}, "domain/train_routine_5fc766.json", "domain")
     cfg = get_cfg()
     assert args.config_file == "", f"This code automatically uses the config file in this directory"
     args.config_file = "configs.yaml"
